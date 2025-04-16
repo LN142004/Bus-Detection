2e524b3e357f80df23193a5e4fc4edd42ddbc803
LN142004 <lakshminarayanan.b2022@vitstudent.ac.in>
2025-04-16 20:50:02 +0530

    Update infer.py

--- src/infer.py
+++ src/infer.py
@@ -1,4 +1,4 @@
 import torch
 
-def load_model(weights_path="yolov5/runs/train/exp/weights/best.pt"):
+def load_model(weights_path="best.pt"):
     return torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path, force_reload=True)
