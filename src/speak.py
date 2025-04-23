6ae2f0d7d2c52cacc3b93ae3a38741351983a270
LN142004 <lakshminarayanan.b2022@vitstudent.ac.in>
2025-04-23 08:46:07 +0530

    Update speak.py

--- src/speak.py
+++ src/speak.py
@@ -1,6 +1,10 @@
 from gtts import gTTS
+import os
 
 def speak(text, filename="detected_number.mp3"):
+    os.makedirs("output", exist_ok=True)
+    path = os.path.join("output", filename)
     tts = gTTS(text=text, lang='en')
-    tts.save(filename)
-    return filename
+    tts.save(path)
+    return path  # <--- return full path
+
